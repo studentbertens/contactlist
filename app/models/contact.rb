281257e73a1526fdb16adda8b97787d8731badd5
studentbertens <alex.bertens@gmail.com>
2015-09-14 20:08:49 -0300

    contacts view

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  has_many :addresses
+  has_many :emails
+  has_many :links
+  has_many :phones
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,5 @@
 class Email < ActiveRecord::Base
+  belongs_to :contacts
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, format: { with: VALID_EMAIL_REGEX }
 end
