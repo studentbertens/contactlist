dc24735b84565fe03826050a1a4aec18567f5488
studentbertens <alex.bertens@gmail.com>
2015-09-14 21:54:43 -0300

    falta edit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,55 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
   def new
     @contact = Contact.new
+    @contact.emails.build
+    @contact.phones.build
+    @contact.links.build
+    @contact.addresses.build
+
   end
 
   def create
-    @Contact = Contact.create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
   end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    if @contact.destroy
+      redirect_to contacts_path
+    else
+      redirect_to contacts_path
+    end
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to contact_path(@contact)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    @emails = @contact.emails.all
+    @phones = @contact.phones.all
+    @links = @contact.links.all
+    @addresses = @contact.addresses.all
+  end
+
+  def contact_params
+
+    params.require(:contact).permit(:name, :note, emails_attributes: [:id, :email], phones_attributes: [:id, :phone], links_attributes: [:id, :link], addresses_attributes: [:id, :address])
+  end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,10 @@
 class Contact < ActiveRecord::Base
-  has_many :addresses
-  has_many :emails
-  has_many :links
-  has_many :phones
+  has_many :addresses, dependent: :destroy
+  has_many :emails, dependent: :destroy
+  has_many :links, dependent: :destroy
+  has_many :phones, dependent: :destroy
+  accepts_nested_attributes_for :addresses, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :emails, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :phones, reject_if: :all_blank, allow_destroy: true
 end
